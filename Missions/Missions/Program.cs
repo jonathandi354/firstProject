c3b491912ed68408b86cf8847392156b52e1c8cb
jonathan diamant <jonathan.diamant1@gmail.com>
2019-03-09 19:22:30 +0200

    first of Missions

---
+++ Missions/Missions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missions
+{
+    public interface IMission
+    {
+        event EventHandler<double> OnCalculate; // An Event of when a mission is activated
+        String Name { get; }
+        String Type { get; }
+        double Calculate(double value);
+    }
+    class SingleMission : IMission
+    {
+        public string Name { get; }
+
+        public string Type { get; }
+
+        public SingleMission(string name)
+        {
+            Name = name;
+            Type = "SingleMission";
+        }
+
+        public event EventHandler<double> OnCalculate;
+
+        public double Calculate(double value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- classes/classes/Program.cs
+++ classes/classes/Program.cs
@@ -25,7 +25,6 @@ namespace classes
             }
             set
             {
-                
                 hash.Add(key, value);
             }
         }
