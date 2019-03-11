0396d902921c565246e93725c7d4323d37025417
jonathan diamant <jonathan.diamant1@gmail.com>
2019-03-11 12:15:30 +0200

    Composed Mission

--- Missions/Missions/Program.cs
+++ Missions/Missions/Program.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace Missions
 {
     
+    class FunctionContainers
+    {
+            private Dictionary<string, Func<double, double>> hash;
+            //private Dictionary<string, EventHandler> hash;
+            public FunctionContainers()
+            {
+                hash = new Dictionary<string, Func<double, double>>();
+                //hash = new Dictionary<string, EventHandler>();
+            }
+            public Func<double, double> this[string key]
+            {
+                get
+                {
+                    return hash[key];
+                }
+                set
+                {
+                    hash.Add(key, value);
+                }
+            }
+    }
+    
     public interface IMission
     {
         event EventHandler<double> OnCalculate; // An Event of when a mission is activated
@@ -17,33 +39,67 @@ namespace Missions
     class SingleMission : IMission
     {
         public string Name { get; }
-        private double result;
+        //private double result;
         public string Type { get; }
         private Func<double, double> func;
 
-        public void MyFunc(object sender, double value)
+        /*public void MyFunc(object sender, double value)
         {
             result = func(value);
-        }
+        }*/
 
         public SingleMission(Func<double, double> func, string name)
         {
             this.func = func;
             Name = name;
             Type = "SingleMission";
-            OnCalculate += MyFunc;
+            //OnCalculate += MyFunc;
         }
 
         public event EventHandler<double> OnCalculate;
 
         public double Calculate(double value)
         {
-            
-            OnCalculate?.Invoke(this, value);
+            double result = func(value);
+            OnCalculate?.Invoke(this, result);
             return result;
-
-
             
+        }
+    }
+
+    class ComposedMission : IMission
+    {
+        public string Name { get; }
+
+        public string Type { get; }
+        private List<Func<double, double>> funcs;
+        public ComposedMission(string name)
+        {
+            Name = name;
+            Type = "ComposedMission";
+            funcs = new List<Func<double, double>>();
+        }
+
+        public event EventHandler<double> OnCalculate;
+        public ComposedMission Add(Func<double ,double> func)
+        {
+            funcs.Add(func);
+
+            return this;
+        }
+        public double Calculate(double value)
+        {
+            double result = value;
+            foreach(var func in funcs)
+            {
+
+                result = func(result);
+            }
+            OnCalculate?.Invoke(this, result);
+            return result;
+            
+                
+                                                  
         }
     }
 
@@ -53,8 +109,18 @@ namespace Missions
         
         static void Main(string[] args)
         {
+            
+            
+            FunctionContainers containers = new FunctionContainers();
+            containers["func1"] = x=>x*2;
+            containers["func2"] = x=>x+3;
 
-
+            SingleMission s = new SingleMission(containers["func1"], "hi");
+            Console.WriteLine(s.Calculate(3));
+            ComposedMission c = new ComposedMission("by");
+            c.Add(containers["func1"]).Add(containers["func2"]);
+            Console.WriteLine(c.Calculate(2));
+            Console.ReadLine();
         }
     }
 }
